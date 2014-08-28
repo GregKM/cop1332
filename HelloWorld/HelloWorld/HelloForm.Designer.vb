2efe92eb931588a28ebf6a2ae1b197c1fa0eaa19
GregKM <gregkmathews@gmail.com>
2014-08-28 03:31:47 -0400

    Created Hello World project.

---
+++ HelloWorld/HelloWorld/HelloForm.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HelloForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(HelloForm))
+        Me.MessageLabel = New System.Windows.Forms.Label()
+        Me.PushButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SpanishButton = New System.Windows.Forms.Button()
+        Me.PrintButton = New System.Windows.Forms.Button()
+        Me.PrintForm1 = New Microsoft.VisualBasic.PowerPacks.Printing.PrintForm(Me.components)
+        Me.SuspendLayout()
+        '
+        'MessageLabel
+        '
+        Me.MessageLabel.AutoSize = True
+        Me.MessageLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.MessageLabel.Location = New System.Drawing.Point(97, 44)
+        Me.MessageLabel.Name = "MessageLabel"
+        Me.MessageLabel.Size = New System.Drawing.Size(0, 20)
+        Me.MessageLabel.TabIndex = 0
+        Me.MessageLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PushButton
+        '
+        Me.PushButton.Location = New System.Drawing.Point(22, 135)
+        Me.PushButton.Name = "PushButton"
+        Me.PushButton.Size = New System.Drawing.Size(75, 23)
+        Me.PushButton.TabIndex = 1
+        Me.PushButton.Text = "English"
+        Me.PushButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(203, 165)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(75, 23)
+        Me.ExitButton.TabIndex = 2
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(200, 220)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(104, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "by Gregory Mathews"
+        '
+        'SpanishButton
+        '
+        Me.SpanishButton.Location = New System.Drawing.Point(22, 165)
+        Me.SpanishButton.Name = "SpanishButton"
+        Me.SpanishButton.Size = New System.Drawing.Size(75, 23)
+        Me.SpanishButton.TabIndex = 4
+        Me.SpanishButton.Text = "Spanish"
+        Me.SpanishButton.UseVisualStyleBackColor = True
+        '
+        'PrintButton
+        '
+        Me.PrintButton.Location = New System.Drawing.Point(203, 135)
+        Me.PrintButton.Name = "PrintButton"
+        Me.PrintButton.Size = New System.Drawing.Size(75, 23)
+        Me.PrintButton.TabIndex = 5
+        Me.PrintButton.Text = "Print"
+        Me.PrintButton.UseVisualStyleBackColor = True
+        '
+        'PrintForm1
+        '
+        Me.PrintForm1.DocumentName = "document"
+        Me.PrintForm1.Form = Me
+        Me.PrintForm1.PrintAction = System.Drawing.Printing.PrintAction.PrintToPrinter
+        Me.PrintForm1.PrinterSettings = CType(resources.GetObject("PrintForm1.PrinterSettings"), System.Drawing.Printing.PrinterSettings)
+        Me.PrintForm1.PrintFileName = Nothing
+        '
+        'HelloForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 242)
+        Me.Controls.Add(Me.PrintButton)
+        Me.Controls.Add(Me.SpanishButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.PushButton)
+        Me.Controls.Add(Me.MessageLabel)
+        Me.Name = "HelloForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hello World by Gregory Mathews"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MessageLabel As System.Windows.Forms.Label
+    Friend WithEvents PushButton As System.Windows.Forms.Button
+    Friend WithEvents ExitButton As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents SpanishButton As System.Windows.Forms.Button
+    Friend WithEvents PrintButton As System.Windows.Forms.Button
+    Friend WithEvents PrintForm1 As Microsoft.VisualBasic.PowerPacks.Printing.PrintForm
+
+End Class
---
+++ HelloWorld/HelloWorld/HelloForm.vb
@@ -0,0 +1,42 @@
+﻿' Project:      Hello World
+' Programmer:   Gregory Mathews
+' Date:         08/28/2014
+' Description:  This project will display a "Hello World"
+'               message in two different languages
+'               and print the form.
+
+Public Class HelloForm
+
+    Private Sub PushButton_Click(sender As Object, e As EventArgs) Handles PushButton.Click
+
+        ' Display the Hello World message.
+
+        MessageLabel.Text = "Hello World"
+
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+
+        ' Exit the project.
+
+        Me.Close()
+
+    End Sub
+
+    Private Sub SpanishButton_Click(sender As Object, e As EventArgs) Handles SpanishButton.Click
+
+        ' Display the Hello World message in Spanish.
+
+        MessageLabel.Text = "Hola Mundo"
+
+    End Sub
+
+    Private Sub Print_Click(sender As Object, e As EventArgs) Handles PrintButton.Click
+
+        ' Print the form on the printer.
+
+        PrintForm1.PrintAction = Printing.PrintAction.PrintToPreview
+        PrintForm1.Print()
+
+    End Sub
+End Class
